56e10aa2abcffee1fa1fbd7dc8d3c136d8c1d32b
Nic Mitchell <nic@nicmitchell.com>
2015-05-09 21:57:38 -0700

    initial layout and animations

--- index.php
+++ index.php
@@ -1,2 +1,53 @@
-<?php
-echo 'hello world!';
+<!DOCTYPE html>
+  <head>  
+    <title>Please Register for ROI-DNA</title>
+    <link rel="stylesheet" href="/bower_components/foundation/css/foundation.min.css">
+    <link rel="stylesheet" href="/bower_components/foundation/css/normalize.min.css">
+    <link rel="stylesheet" href="/bower_components/animate.css/animate.min.css">
+    <link rel="stylesheet" href="/css/custom.css">
+  </head>
+  <body>
+    <header>
+      <nav class="top-bar" data-topbar role="navigation">
+        <ul class="title-area">
+          <li class="name">
+            <h1><a href="#">Register for ROI-DNA</a></h1>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <div class="row">
+      <section class="main small-6 small-centered columns">
+        <img class="animated fadeInDown logo" src="http://www.roidna.com/wordpress/wp-content/themes/roidna/imgs/logo-white.svg">
+        <form data-abide class="animated bounceInUp">
+          <fieldset>
+            <legend>Registration</legend>
+            
+            <div class="name-field animated flipInX">
+              <label>Your name <small>required</small>
+                <input type="text" required pattern="[a-zA-Z]+">
+              </label>
+              <small class="error animated rubberBand ">Name is required and must be a string.</small>
+            </div>
+            <div class="email-field animated flipInX">
+              <label>Email <small>required</small>
+                <input type="email" required>
+              </label>
+              <small class="error animated rubberBand">An email address is required.</small>
+            </div>
+            <button type="submit" class="register animated bounceIn">Register</button>
+            <button type="submit" class="reset animated bounceIn">Reset Password</button>
+          </fieldset>
+        </form>
+      </section>
+  </div>
+
+    <footer class="column bottom-bar">Footer Stuff</footer>
+  <script src="/bower_components/foundation/js/vendor/jquery.js"></script>
+  <script src="/bower_components/foundation/js/vendor/fastclick.js"></script>
+  <script src="/bower_components/foundation/js/foundation.js"></script>
+  <script src="/bower_components/foundation/js/vendor/modernizr.js"></script>
+  <script src="http://localhost:35729/livereload.js"></script>
+  <script>$(document).foundation();</script>
+  </body>
+</html>
